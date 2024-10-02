04c6ccf8fdadbba24cfb360829f7625ef7309e10
yash savani <yashsavani1585@gmail.com>
2024-10-02 14:42:43 +0530

    Update App.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,6 +1,6 @@
 
 import Navbar from './components/navbar/Navbar'
-import Sidebar from './components/sidebar/sidebar'
+import Sidebar from './components/sidebar/Sidebar';
 import { Route, Routes } from'react-router-dom'
 import Add from './pages/Add/Add'
 import List from './pages/List/List'
