8b3e055cd26a2381ea73567c6ac1e50b37822a84
yash savani <yashsavani1585@gmail.com>
2024-10-02 14:39:15 +0530

    Update App.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -9,7 +9,7 @@ import { ToastContainer} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
-  const url = "http://localhost:3000"
+  const url = "https://food-app-lgji.onrender.com"
 
   return (
     
