bbf164f5be9d225eca10e8666bb9e49e1a04da8e
yash savani <yashsavani1585@gmail.com>
2024-10-03 12:34:09 +0530

    Update assets.js

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -6,7 +6,7 @@ import upload_area from './upload_area.png'
 import parcel_icon from './parcel_icon.png'
 
 export const url = 'https://food-app-lgji.onrender.com'
-export const currency='₹'
+
 
 export const assets ={
     logo,
